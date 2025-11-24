d0d0cb276b1d03e6f40a1ac100bbd1889e35f628
Vancesca Dinh <dinh.vancesca@gmail.com>
2025-11-24 10:23:40 +0100

    add: hw4, q4

--- wikiagent/main.py
+++ wikiagent/main.py
@@ -1,25 +1,42 @@
 import wikiagent
-# import asyncio
+import asyncio
+from agent_logging import log_streamed_run, save_log
 
-agent = wikiagent.create_agent()
-agent_callback = wikiagent.NamedCallback(agent)
 
-async def run_agent(user_prompt: str):
+async def main():
 
-    results = await agent.run(
-        user_prompt=user_prompt,
-        event_stream_handler=agent_callback
-    )
+    user_input = "where do cabybaras live?"
 
-    return results
+    agent = wikiagent.create_agent()
+    agent_callback = wikiagent.NamedCallback(agent)
 
-# def run_agent_sync(user_prompt:str):
-#     return asyncio.run(run_agent(user_prompt))
+    previous_text = ""
 
-# result = asyncio.run(run_agent('where do cabybaras live?'))
-# print(result.output)
+    async with agent.run_stream(user_input, event_stream_handler=agent_callback) as result:
+        async for item, last in result.stream_responses(debounce_by=0.01):
+            for part in item.parts:
+                if not hasattr(part, "tool_name"):
+                    continue
+                if part.tool_name != "final_result":
+                    continue
 
-# def main():
+                current_text = part.args
+                delta = current_text[len(previous_text):]
+                # print(delta, end="", flush=True)
+
+                previous_text = current_text
+        log_entry = await log_streamed_run(agent, result)
+        save_log(log_entry)
+
+
+    # results = await agent.run(
+    #     user_prompt=user_prompt,
+    #     event_stream_handler=agent_callback
+    # )
+    # return results
+
+
+
     
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    asyncio.run(main())
